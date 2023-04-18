e54d6a65c8b8f3d15a0cbd0269db62cfdf69ba83
Nirav Sakariya <118878708+NiravSakariya@users.noreply.github.com>
2023-04-18 15:15:24 +0530

    Delete MODULE-4 ADVANCE PHP/03-03-2023 webservices directory

--- MODULE-4 ADVANCE PHP/03-03-2023 webservices/state_list.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$state_list=array("gj"=>"gujrat","up"=>"uttar pradesh","mp"=>"madhya pradesh","rj"=>"rajsthan","mh"=>"mahrastra");
-//print_r($country_list);
-echo json_encode($state_list);
-
-?>
