5f33006211885ad26fb5339e74d29ff0118cb36d
Nirav Sakariya <118878708+NiravSakariya@users.noreply.github.com>
2023-04-18 15:24:15 +0530

    Add files via upload

---
+++ MODULE-4 ADVANCE PHP/5. Session/15-03-2023  session/index.php
@@ -0,0 +1,11 @@
+<?php 
+require_once("header.php");
+require_once("navigation.php");
+require_once("slider.php");
+require_once("content.php");
+require_once("footer.php");
+require_once("login.php");
+require_once("register.php");
+
+?>
+
---
+++ MODULE-4 ADVANCE PHP/5. Session/17-03-2023  session/footer.php
@@ -0,0 +1,7 @@
+<div class="container-fluid bg-dark text-white p-5">
+    <center>
+        <p>copyright @allright reserved</p>
+    </center>
+
+
+</div>
